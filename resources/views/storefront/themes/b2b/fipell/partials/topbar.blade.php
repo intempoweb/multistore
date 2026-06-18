80588ef67c587110c3d787bd1befcaafe5131ad1
mperu85 <mperu85@gmail.com>
2026-06-18 16:22:09 +0200

    layout fipell header

--- resources/views/storefront/themes/b2b/fipell/partials/topbar.blade.php
+++ resources/views/storefront/themes/b2b/fipell/partials/topbar.blade.php
@@ -7,20 +7,9 @@
     $agentContextId = $agentContextId ?? (string) request('agent_context', '');
     $contextParams = $contextParams ?? ($agentContextId !== '' ? ['agent_context' => $agentContextId] : []);
 
-    $storeEmail = $store->email
-        ?? $store->support_email
-        ?? $store->customer_service_email
-        ?? null;
-
-    $storePhone = $store->phone
-        ?? $store->telephone
-        ?? $store->customer_service_phone
-        ?? null;
-
-    $storeVat = $store->vat_number
-        ?? $store->piva
-        ?? $store->vat
-        ?? null;
+    $storeEmail = $store->email ?? $store->support_email ?? $store->customer_service_email ?? null;
+    $storePhone = $store->phone ?? $store->telephone ?? $store->customer_service_phone ?? null;
+    $storeVat = $store->vat_number ?? $store->piva ?? $store->vat ?? null;
 
     $documentsUrl = Route::has('storefront.account.documents.index')
         ? route('storefront.account.documents.index', $contextParams)
@@ -28,41 +17,88 @@
 @endphp
 
 <div class="storefront-topbar fipell-topbar">
-    <div class="container-fluid fipell-topbar-inner">
-        <a
-            href="{{ route('storefront.home', $contextParams) }}"
-            class="fipell-topbar-brand"
-            aria-label="{{ $storeName }}"
-        >
-            @if($storeLogo)
-                <img src="{{ $storeLogo }}" alt="{{ $storeName }}" class="fipell-topbar-logo">
-            @else
-                <span class="fipell-topbar-logo-fallback">{{ mb_substr($storeName, 0, 1) }}</span>
-            @endif
-        </a>
+    <div class="container-fluid fipell-shell">
+        <div class="fipell-topbar-inner">
 
-        <div class="fipell-topbar-links">
-            @auth('customer')
-                @if(Route::has('storefront.wishlist.index'))
-                    <a href="{{ route('storefront.wishlist.index', $contextParams) }}">Preferiti</a>
+            <div class="fipell-topbar-left">
+                <a href="{{ route('storefront.home', $contextParams) }}" class="fipell-topbar-brand" aria-label="{{ $storeName }}">
+                    @if($storeLogo)
+                        <img src="{{ $storeLogo }}" alt="{{ $storeName }}" class="fipell-topbar-logo" loading="eager" decoding="async">
+                    @else
+                        <span class="fipell-logo-fallback">{{ mb_substr($storeName, 0, 1) }}</span>
+                    @endif
+                </a>
+
+                <div class="fipell-topbar-contacts">
+                    @if($storeEmail)
+                        <a href="mailto:{{ $storeEmail }}">
+                            <i class="fa-solid fa-envelope"></i>
+                            <span>{{ $storeEmail }}</span>
+                        </a>
+                    @endif
+
+                    @if($storePhone)
+                        <a href="tel:{{ preg_replace('/\s+/', '', (string) $storePhone) }}">
+                            <i class="fa-solid fa-phone"></i>
+                            <span>{{ $storePhone }}</span>
+                        </a>
+                    @endif
+
+                    @if($storeVat)
+                        <span>
+                            <i class="fa-solid fa-receipt"></i>
+                            <span>P. IVA {{ $storeVat }}</span>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="fipell-topbar-links">
+                @if(Route::has('storefront.catalog.index'))
+                    <a href="{{ route('storefront.catalog.index', $contextParams) }}">
+                        <i class="fa-solid fa-boxes-stacked"></i>
+                        Catalogo
+                    </a>
                 @endif
 
-                @if($isB2b)
-                    <a href="{{ $documentsUrl }}">Documenti</a>
-                @endif
+                @auth('customer')
+                    @if(Route::has('storefront.wishlist.index'))
+                        <a href="{{ route('storefront.wishlist.index', $contextParams) }}">
+                            <i class="fa-regular fa-heart"></i>
+                            Preferiti
+                        </a>
+                    @endif
 
-                @if(Route::has('storefront.account.index'))
-                    <a href="{{ route('storefront.account.index', $contextParams) }}">Area cliente</a>
-                @endif
+                    @if($isB2b)
+                        <a href="{{ $documentsUrl }}">
+                            <i class="fa-solid fa-file-lines"></i>
+                            Documenti
+                        </a>
+                    @endif
 
-                @if(Route::has('storefront.logout'))
-                    <a href="{{ route('storefront.logout') }}">Logout</a>
-                @endif
-            @else
-                @if(Route::has('storefront.login'))
-                    <a href="{{ route('storefront.login', $contextParams) }}">Accedi</a>
-                @endif
-            @endauth
+                    @if(Route::has('storefront.account.index'))
+                        <a href="{{ route('storefront.account.index', $contextParams) }}">
+                            <i class="fa-solid fa-user"></i>
+                            Area cliente
+                        </a>
+                    @endif
+
+                    @if(Route::has('storefront.logout'))
+                        <a href="{{ route('storefront.logout') }}">
+                            <i class="fa-solid fa-right-from-bracket"></i>
+                            Logout
+                        </a>
+                    @endif
+                @else
+                    @if(Route::has('storefront.login'))
+                        <a href="{{ route('storefront.login', $contextParams) }}" class="fipell-topbar-login">
+                            <i class="fa-solid fa-right-to-bracket"></i>
+                            Accedi
+                        </a>
+                    @endif
+                @endauth
+            </div>
+
         </div>
     </div>
 </div>
