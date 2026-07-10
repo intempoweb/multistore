f6b5141f66684fe24b94b6d23de83f30b6b5a79c
mperu85 <mperu85@gmail.com>
2026-07-10 20:22:52 +0200

    corretto font per ciak

--- resources/views/storefront/themes/b2c/ciak/layout.blade.php
+++ resources/views/storefront/themes/b2c/ciak/layout.blade.php
@@ -8,7 +8,7 @@
     @include('storefront.base.partials.seo')
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
