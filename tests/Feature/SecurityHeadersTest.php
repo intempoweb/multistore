f54e4bfd2017443b6d8ae40e6099ea632a43806f
mperu85 <mperu85@gmail.com>
2026-09-25 12:57:36 +0200

    Allow Meta Pixel beacon CSP endpoints

--- tests/Feature/SecurityHeadersTest.php
+++ tests/Feature/SecurityHeadersTest.php
@@ -17,5 +17,7 @@ class SecurityHeadersTest extends TestCase
         $response->assertHeader('X-Frame-Options', 'SAMEORIGIN');
         $this->assertStringContainsString("frame-ancestors 'self'", $response->headers->get('Content-Security-Policy'));
         $this->assertStringContainsString('https://connect.facebook.net', $response->headers->get('Content-Security-Policy'));
+        $this->assertStringContainsString('form-action \'self\' https://www.paypal.com https://www.sandbox.paypal.com https://www.facebook.com', $response->headers->get('Content-Security-Policy'));
+        $this->assertStringContainsString('frame-src \'self\' https://js.stripe.com https://hooks.stripe.com https://www.paypal.com https://www.sandbox.paypal.com https://www.google.com https://www.facebook.com', $response->headers->get('Content-Security-Policy'));
     }
 }
